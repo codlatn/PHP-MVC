3023d3f789b1bdc6139225e1a7798b7e32b74e4d
FAHAD AL-Zahrani <admin@codlatn.com>
2023-04-06 01:15:53 +0300

    update - aliases

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
-
+//DB
 return [
     'server' => 'localhost',
     'dbname' => 'phpmvc',
@@ -7,4 +7,23 @@ return [
     'dbpass' => ''
    
 ];
+
+
+//aliases
+//  return [
+//         'request' => 'System\\Http\\Request',
+//         'response' => 'System\\Http\\Response',
+//         'cookie' => 'System\\Cookie\\Cookie',
+//         'session' => 'System\\Session\\Session',
+//         'route' => 'System\\Route',
+//         'html' => 'System\\Html',
+//         'db' => 'System\\Database',
+//       //'model' => 'System\\Model',
+//         'load' => 'System\\Loader',
+//         'view' => 'System\\View\viewFactory',
+//         'url' => 'System\\Url',
+//         'twig' => 'System\\View\Template\\Twig',
+//         'validation' => 'System\\Validation',
+      
+//     ];
 
